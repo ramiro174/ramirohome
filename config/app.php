84b6d6f89f70198306f204e1daa1b82dba993b58
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-10-25 12:17:52 -0500

    ad

--- config/app.php
+++ config/app.php
@@ -161,7 +161,9 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Laravel\Passport\PassportServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
+        
         /*
          * Package Service Providers...
          */
