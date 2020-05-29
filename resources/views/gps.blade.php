ddd3c47a0d51f318c6e3b65697928e51e1fa31a5
Ramiro Esquivel Duran <ramiro174@gmail.com>
2020-05-29 12:09:22 -0500

    ad

--- resources/views/gps.blade.php
+++ resources/views/gps.blade.php
@@ -55,7 +55,6 @@
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script>
-
     $.ajax({
         accept:'application/json',
         url:'/api/recuperargps',
@@ -109,7 +108,7 @@
 
     }
 </script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgPHJgYDvFuQiUtONDSS-v0WlZ0fD6-Bk&callback=initMap">
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDL184q65HWjgvsebXvT7ISNn8SnVsHZc">
 
 </script>
 
