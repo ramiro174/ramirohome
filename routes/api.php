177cf1b9ed6878f31cf65ccc92d39d399d2f9dcc
Ramiro Esquivel Duran <ramiro174@gmail.com>
2020-11-18 09:48:56 -0600

    ad

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@
             ["nombre" =>"Juan",
             "edad" =>"18",
             "Sexo" =>"M",
-            "Descripcion" =>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            "descripcion" =>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
